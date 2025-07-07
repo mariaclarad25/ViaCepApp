cc4ec46193da9bba7723080ac3d453dddde80249
Maria Clara <mariaclara.dg25@gmail.com>
2025-07-07 09:11:12 -0300

    Initial Commit

---
+++ AppViaCEP/AppViaCEPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppViaCEPApp.swift
+//  AppViaCEP
+//
+//  Created by Maria Clara Dias on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AppViaCEPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppViaCEP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppViaCEP
+//
+//  Created by Maria Clara Dias on 07/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
